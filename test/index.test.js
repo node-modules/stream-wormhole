53ec1b21d0847c5c2d32391f60302cc9e96461f4
fengmk2 <fengmk2@gmail.com>
2018-08-14 10:55:13 +0800

    feat: support piped read stream
    should always call unpipe first.

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,77 @@
 'use strict';
 
 const assert = require('assert');
+const path = require('path');
 const fs = require('fs');
+const Writable = require('stream').Writable;
 const sendToWormhole = require('..');
 
-describe('index.test.js', () => {
+const bigtext = path.join(__dirname, 'fixtures/big.txt');
+
+describe('test/index.test.js', () => {
   it('should work with read stream', () => {
-    const stream = fs.createReadStream(__filename);
+    const stream = fs.createReadStream(bigtext);
     return sendToWormhole(stream);
   });
 
+  it('should call multi times work', () => {
+    const stream = fs.createReadStream(bigtext);
+    return sendToWormhole(stream).then(() => {
+      return sendToWormhole(stream).then(() => {
+        return sendToWormhole(stream);
+      });
+    });
+  });
+
+  it('should work with read stream after pipe', done => {
+    let writeSize = 0;
+    class PauseStream extends Writable {
+      _write(/* chunk, encoding, callback */) {
+        console.log('PauseStream1 write buffer size: %d', arguments[0].length);
+        writeSize += arguments[0].length;
+        // do nothing
+      }
+    }
+
+    const stream = fs.createReadStream(bigtext);
+    stream.pipe(new PauseStream());
+    // mock delay
+    setTimeout(() => {
+      assert(writeSize > 0);
+      sendToWormhole(stream).then(done);
+    }, 100);
+  });
+
+  it('should call multi times work with read stream after pipe', done => {
+    let writeSize = 0;
+    class PauseStream extends Writable {
+      _write(/* chunk, encoding, callback */) {
+        console.log('PauseStream2 write buffer size: %d', arguments[0].length);
+        writeSize += arguments[0].length;
+        // do nothing
+      }
+    }
+
+    const stream = fs.createReadStream(bigtext);
+    stream.pipe(new PauseStream());
+    // mock delay
+    setTimeout(() => {
+      assert(writeSize > 0);
+      sendToWormhole(stream).then(() => {
+        sendToWormhole(stream).then(() => {
+          sendToWormhole(stream).then(done);
+        });
+      });
+    }, 100);
+  });
+
   it('should not throw error by default when stream error', () => {
-    const stream = fs.createReadStream(__filename + '-not-exists');
+    const stream = fs.createReadStream(bigtext + '-not-exists');
     return sendToWormhole(stream);
   });
 
   it('should throw error when stream error', done => {
-    const stream = fs.createReadStream(__filename + '-not-exists');
+    const stream = fs.createReadStream(bigtext + '-not-exists');
     sendToWormhole(stream, true).catch(err => {
       assert.equal(err.code, 'ENOENT');
       done();
@@ -24,7 +79,7 @@ describe('index.test.js', () => {
   });
 
   it('should pass ended', done => {
-    const stream = fs.createReadStream(__filename);
+    const stream = fs.createReadStream(bigtext);
     stream.resume();
     stream.on('end', () => {
       sendToWormhole(stream).then(done);
@@ -53,7 +108,17 @@ describe('index.test.js', () => {
   });
 
   it('should work on co', function* () {
-    const stream = fs.createReadStream(__filename);
+    const stream = fs.createReadStream(bigtext);
+    yield sendToWormhole(stream);
+    assert.equal(stream.readable, false);
+    assert(stream.destroyed);
+    // again should work
+    yield sendToWormhole(stream);
+    assert.equal(stream.readable, false);
+    assert(stream.destroyed);
+    yield sendToWormhole(stream);
+    assert.equal(stream.readable, false);
+    assert(stream.destroyed);
     yield sendToWormhole(stream);
     assert.equal(stream.readable, false);
     assert(stream.destroyed);
